1b18b52a07fd48baf7a03209b401dd0747cdf231
Camilo Porto <camiloporto@gmail.com>
2015-12-01 22:34:36 -0300

    adicionado FIXME do proximo passo a ser dado

---
+++ app/src/test/java/br/com/camiloporto/marmitex/android/MarmitexApplicationTest.java
@@ -0,0 +1,15 @@
+package br.com.camiloporto.marmitex.android;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+/**
+ * Created by camiloporto on 01/12/15.
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class MarmitexApplicationTest {
+
+    //// FIXME: Criar Uma classe que extenda ApplicationContext e faça a interacao entre GUI e MarmitariaService e sirva para compartilhar informacao usada pelas Actiities
+}
